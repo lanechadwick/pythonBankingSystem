4b3796c40c060e9db07765621acc851a894e0158
lanechadwick <57504026+lanechadwick@users.noreply.github.com>
2020-12-09 14:16:20 -0700

    Add files via upload

---
+++ oopProject.py
@@ -0,0 +1,46 @@
+#Banking System using OOP
+#Parent Class : User
+#Has a function to show user details
+#Child Class: Bank
+#Stores details about the account balance
+#Stores details about the amount
+#Allows for deposits, withdraw view_balance
+
+
+#Parent Class
+class User():
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def show_details(self):
+        print("Personal Details")
+        print("")
+        print("Name ", self.name)
+        print("Age ",  self.age)
+        print("Gender", self.gender)
+
+
+#Child Class
+class Bank(User):
+    def __init__(self,name,age,gender,):
+        super().__init__(name,age,gender)
+        self.balance = 0
+
+    def deposit(self,amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Account balance has been updated: $", self.balance)
+
+    def withdraw(self,amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient Funds | Balance Avalible: $", self.balance)
+        else:
+                self.balance = self.balance - self.amount
+                print("Account balance has been updated: $", self.balance)
+
+    def view_balance(self):
+        self.show_details()
+        print("Account balance: $", self.balance)
